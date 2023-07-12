b15b439e4ccebd8c408ac9de002c7a19668cc58e
RenatoNunes22 <renatonunes0011@hotmail.com>
2023-07-11 21:58:45 -0300

    fix:remove-db-connection

--- src/config/dbConnect.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose"
-
-//CONEXÃO COM O BANCO DE DADOS
-mongoose.connect("mongodb+srv://renatonunes:root@aluranode.ezhgk78.mongodb.net/aluranode");
-let db = mongoose.connection;
-
-export default db;
